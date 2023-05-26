045011885809a1a71f5442a7ed2a29c9989f4817
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2023-05-26 13:02:48 +0100

    Create example2.js

---
+++ Public/Scripts/Hacklet/example2.js
@@ -0,0 +1,9 @@
+// -- Hacklet --
+// @name         Get All Blooks
+// @version      1.2
+// @author       Hacklet
+// @urls         ["https://www.blooket.com", "https://play.blooket.com/*"]
+// -- Hacklet -- 
+
+console.log("Working, Yay!");
+document.body.innerHTML = `<h1>hi</h1>`;
